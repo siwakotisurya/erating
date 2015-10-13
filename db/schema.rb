43baa4b35db426db28ed45e379165fdd460ffff8
siwakotisurya <siwakotisurya@outlook.com>
2015-10-13 10:39:35 +0545

    migration generated to add extra image field for paper clip in users table and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012153909) do
+ActiveRecord::Schema.define(version: 20151013045253) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                            default: "", null: false
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20151012153909) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
