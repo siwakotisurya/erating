4c374f586c675f8b78bcd9ed6d4694a685242bdf
siwakotisurya <siwakotisurya@outlook.com>
2015-10-13 10:42:56 +0545

    validation added for paperclip gemes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]       
 end
