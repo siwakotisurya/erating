f94102b44f66de7f9cf091b24f0797792b6f615d
siwakotisurya <siwakotisurya@outlook.com>
2015-10-14 11:29:51 +0545

    added validation for phone number and address in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+	validates_length_of :phone_number, :numericality=>{:less_than_or_equal_to=>10}
+	validates_format_of :address, :with => /^[a-zA-Z\d\s]*$/,:message => "can only contain letters and numbers."
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]       
  
